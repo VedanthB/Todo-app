edf10b7fda8ebb4ed1bd67c6417713674327477e
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-02-12 20:22:33 +0530

    added addTodo state and the input part

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -1,15 +1,29 @@
-import React, { useState }from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [todos, setTodos] = useState(['take dogs out','anything'])
+  const [todos, setTodos] = useState(['take dogs out','anything']);
+  const [input, setinput] = useState('');
+  console.log('🧜🏼‍♀️', input);
+
+  const addTodo = (event) => {
+    //this will fire off when we click the button
+    event.preventDefault();   // prevents RELOAD
+    console.log('🧜🏼‍♀️', 'IM working');
+    setTodos([...todos,input]);  //using a spread
+    setinput('')  // clears the input box after submit
+  }
 
   return (
     <div className="App">
      <h1>hello world</h1>
-     <input/>
-     <button>ADD ToDo</button>
 
+     <form>
+       <input value={input} onChange = {event => setinput(event.target.value)}/>
+       <button onClick={addTodo} >ADD ToDo</button>
+     </form>
+      
+      
       <ul>
         {todos.map(todo => (  //arrow function and jsx
           <li>{todo}</li>
