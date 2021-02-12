a83877d329a37d6a0d41bd3ece25ab4fe170a304
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-02-12 17:50:34 +0530

    useState for todos

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -1,10 +1,22 @@
-
+import React, { useState }from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState(['take dogs out','anything'])
+
   return (
     <div className="App">
      <h1>hello world</h1>
+     <input/>
+     <button>ADD ToDo</button>
+
+      <ul>
+        {todos.map(todo => (  //arrow function and jsx
+          <li>{todo}</li>
+        ))}
+        </ul>
+
+
     </div>
   );
 }
